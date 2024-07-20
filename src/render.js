6242e7b5adbacb9131f5425ddc0058b2e36dce3e
kurorinto <394460456@qq.com>
2024-07-20 18:09:23 +0800

    feat: add code that will reproduce the bug

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 import './render.css'
 
 export function render() {
+    // bg-[url(something)]
     const el = document.createElement('div')
     el.classList.add('text')
     document.getElementsByTagName('body')[0].appendChild(el)
